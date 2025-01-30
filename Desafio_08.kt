b981b669116a9dad04f29b5ede424b8e6ed9c67a
Saymon Botelho <82913820+saymonbot@users.noreply.github.com>
2025-01-30 10:50:36 -0300

    Update Desafio_08.kt

--- Desafio_08.kt
+++ Desafio_08.kt
@@ -1,1 +1,17 @@
+fun main() {
+    val titulo = readLine() ?: ""
+    val autor = readLine() ?: ""
+    
+    val slugTitulo = titulo.generateSlug()
+    val slugAutor = autor.generateSlug()
+    
+    println("Slug gerado para o livro:")
+    
+    println("${slugTitulo}_${slugAutor}") 
+   
+}
 
+fun String.generateSlug(): String {
+    //TODO("Lógica para que qualquer String tenha a capacidade de gerar uma slug (seguindo-este-padrao).")
+    return ((this.replace(' ', '-')).replace(".", "")).lowercase()
+}
